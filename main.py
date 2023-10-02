296392f332b5a1ecdc06b57839a82d39fee3a881
ArtsemDev <thommy.python.dev@gmail.com>
2023-10-02 12:33:37 +0300

    ///

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-print("hello hell")
-print("belhard")
-def foo(): ...
-print("goodbye world")
-print("hello pycharm")
-print()
-class A: ...
-print("bye pycharm")
-print("hello pycharm")
-print("lorem ipsum")
-print()
+def bar():
+    def baz():
+        ...
+    return baz
