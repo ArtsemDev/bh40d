78b6f9fb30941a3489bd282f9094880a7fb87d13
ArtsemDev <thommy.python.dev@gmail.com>
2023-10-02 12:42:50 +0300

    Revert "add input"
    This reverts commit 0ca3c59f

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from itertools import zip_longest
 
-print(len('sdfs'))
-b = input("conflict")
-a = input("conflict")
-c = input("conflict")
+a = input("world")
 print("python")
 print("python")
 print("python")
@@ -15,12 +12,6 @@ print(zip_longest)
 
 
 def foo(): ...
-
-
-def bar(): ...
-
-
-def baz(): ...
 
 
 print("java")
